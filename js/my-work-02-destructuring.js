03066b75664c1fffe29029e77919c889d1bafe79
Andrey Kasperovych <DREDer2005@ukr.net>
2020-09-24 17:46:33 +0300

    Update my-work-02-destructuring.js

--- js/my-work-02-destructuring.js
+++ js/my-work-02-destructuring.js
@@ -4,22 +4,56 @@
  * - Имя переменной отличное от имени свойства
  */
 
-const playlist = {
-    name: 'Мой супер плейлист',
-    rating: 5,
-    tracks: ['трек-1', 'трек-2', 'трек-3'],
-    trackCount: 3,
+// const playlist = {
+//     name: 'Мой супер плейлист',
+//     rating: 5,
+//     tracks: ['трек-1', 'трек-2', 'трек-3'],
+//     trackCount: 3,
+// };
+
+// const { name, rating, tracks, trackCount, corsar = 'anonymus' } = playlist;
+// console.log(rating, tracks, corsar);
+
+// console.log(
+//     playlist.name,
+//     playlist.rating,
+//     playlist.tracks,
+//     playlist.trackCount,
+// );
+
+// const {
+//     name,
+//     rating,
+//     tracks,
+//     trackCount,
+//     trackCount: numberTrack,
+//     author = [1, 3, 5],
+// } = playlist;
+
+// console.log(name, rating, tracks, trackCount, numberTrack, author);
+
+// console.log(playlist);
+
+/*
+ * Глубокая деструктуризация
+ */
+
+const profile = {
+    name: 'Jacques Gluke',
+    tag: 'jgluke',
+    location: 'Ocho Rios, Jamaica',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
+    stats: {
+        followers: 5603,
+        views: 4827,
+        likes: 1308,
+    },
 };
 
-const {
-    name,
-    rating,
-    tracks,
-    trackCount,
-    trackCount: numberTrack,
-    author = [1, 3, 5],
-} = playlist;
+const { stats } = profile;
 
-console.log(name, rating, tracks, trackCount, numberTrack, author);
+console.log(stats);
 
-console.log(playlist);
+const { views } = stats;
+
+console.log(views);
